661e3732aecffb529ffc9acffe32f21642a73c5e
RomanMilishchuk <milromchuk@gmail.com>
2019-10-10 22:42:31 +0300

    Added WinAPI support

--- main.cpp
+++ main.cpp
@@ -6,53 +6,73 @@
 #include <unistd.h>
 #include <cctype>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <winnt.h>
+#endif
+
+#ifndef _WIN32
+typedef BYTE char
+typedef DWORD size_t
+#endif
 const size_t BUFFER_SIZE = 4096;
-char buf[BUFFER_SIZE + 1];
-char buf_hex[BUFFER_SIZE * 4 + 1];
+BYTE buf[BUFFER_SIZE + 1];
+BYTE buf_hex[BUFFER_SIZE * 4 + 1];
 
 //#define USING_LIB_C
 #ifdef USING_LIB_C
 #define FILE_DESC FILE*
-#else
+#elif __linux__
 #define FILE_DESC int
+#elif _WIN32
+#define FILE_DESC HANDLE
 #endif
 
-int stdout_write(const char *buffer, ssize_t size) {
+
+int stdout_write(const BYTE *buffer, DWORD size) {
     ssize_t written_bytes = 0;
     while (written_bytes < size) {
 #ifdef USING_LIB_C
         ssize_t written_chunk = fwrite(buffer, sizeof(char), size, stdout);
         if (ferror(stdout)) {
-#else
-        ssize_t written_chunk = write(STDOUT_FILENO, buffer, size);
+#elif __linux__
+        ssize_t written_chunk = write(STDOUT_FILENO, buffer, static_cast<unsigned int>(size));
         if (written_chunk == -1) {
             if (errno == EINTR)
                 continue;
+#elif _WIN32
+        DWORD written_chunk;
+        HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (!WriteFile(hStdOut, buffer, size, &written_chunk, nullptr)) {
 #endif
             perror("Can`t write to stdout");
             exit(3);
         } else {
-            written_bytes += written_chunk;
+            written_bytes += static_cast<ssize_t>(written_chunk);
         }
     }
+
     return 0;
 }
 
-int file_read(FILE_DESC fd, char *buf, size_t sz) {
+int file_read(FILE_DESC fd, BYTE *buf, DWORD sz) {
     while (true) {
 #ifdef USING_LIB_C
         size_t read_n = fread(buf, sizeof(char), BUFFER_SIZE, fd);
         if (ferror(fd) && !feof(fd)) {
-#else
-            ssize_t read_n = read(fd, buf, BUFFER_SIZE);
-            if (read_n == -1) {
-                if (errno == EINTR)
-                    continue;
+#elif __linux__
+        ssize_t read_n = read(reinterpret_cast<int>(fd), buf, BUFFER_SIZE);
+        if (read_n == -1) {
+            if (errno == EINTR)
+                continue;
+#elif _WIN32
+        DWORD read_n;
+        if (!ReadFile(fd, buf, BUFFER_SIZE, &read_n, NULL)) {
 #endif
             perror("Cannot read file");
             exit(4);
         } else {
-            return read_n;
+            return static_cast<int>(read_n);
         }
     }
 
@@ -113,34 +133,27 @@ po::variables_map parse_args(int argc, char **argv) {
     return vm;
 }
 
-#ifdef USING_LIB_C
 
-std::vector<FILE *> open_files(std::vector<std::string> &files) {
-    std::vector<FILE *> fds;
+std::vector<FILE_DESC> open_files(std::vector<std::string> &files) {
+    std::vector<FILE_DESC> fds;
     for (auto &file_name : files) {
+#ifdef USING_LIB_C
         auto fd = fopen(file_name.c_str(), "r");
         if (fd == nullptr) {
-            perror("Cannot open file");
-            exit(1);
-        }
-        fds.push_back(fd);
-    }
-    return fds;
-}
-
-#else
-std::vector<int> open_files(std::vector<std::string> &files) {
-    std::vector<int> fds;
-    for (auto &file_name : files) {
+#elif __linux__
         int fd = open(file_name.c_str(), O_RDONLY);
         if (fd == -1) {
+#elif _WIN32
+        auto fd = CreateFile(file_name.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY,
+                             nullptr);
+        if (fd == INVALID_HANDLE_VALUE) {
+#endif
             perror("Cannot open file");
         }
         fds.push_back(fd);
     }
     return fds;
 }
-#endif
 
 int main(int argc, char **argv) {
     auto vm = parse_args(argc, argv);
@@ -154,11 +167,13 @@ int main(int argc, char **argv) {
     }
     auto fds = open_files(input_files);
     for (auto fd : fds) {
-        fd_to_stdout(fd, vm.count("show-all"));
+        fd_to_stdout(fd, static_cast<bool>(vm.count("show-all")));
 #ifdef USING_LIB_C
         fclose(fd);
-#else
+#elif __linux__
         close(fd);
+#elif _WIN32
+        CloseHandle(fd);
 #endif
     }
     return 0;
